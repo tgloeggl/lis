e6f0b2ea959f2478bfa548bef7a7d123ecb839e7
Till Glöggler <till.gloeggler@uni-osnabrueck.de>
2010-03-23 15:28:47 +0100

    clip percentage display

--- app/views/_percentage.php
+++ app/views/_percentage.php
@@ -1,7 +1,7 @@
 <?
 $width = 170;
 if ($cur == 0) $percent = 0;
-else $percent = floor($width / ($max / $cur));
+else $percent = min($width, floor($width / ($max / $cur)));
 ?>
 <div class="percentage" title="<?= Functions::formatNumber($cur) ?> von <?= Functions::formatNumber($max) ?> belegt">
 	<div class="filled" style="width: <?= $percent ?>px"></div>
